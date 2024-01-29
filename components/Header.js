f2f6e9b60e9c78d985d0942c2b2765498ba10137
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2024-01-29 15:11:41 +0100

    working on page

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div className='flex flex-row-reverse items-center justify-end mb-6'>
-      <h1 className='text-32'>Dog App</h1>
-      <Image src="/images/dog.svg" alt="Example SVG" width={100} height={100} />
-    </div>
+    <Link href="/">
+      <a className='w-fit block'>
+        <div className='flex flex-row-reverse items-center justify-end mb-6'>
+        <h1 className='text-32'>Dog App</h1>
+        <Image src="/images/dog.svg" alt="Example SVG" width={100} height={100} />
+        </div>
+      </a>
+      </Link>
   );
 };
 
